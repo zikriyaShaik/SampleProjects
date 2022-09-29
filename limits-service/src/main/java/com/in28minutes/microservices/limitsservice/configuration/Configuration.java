62628d0328c79b5cdb475fe85de8d5e9e8a19146
ZIKRIYA_SHAIK <zikriya23@gmail.com>
2022-09-29 21:59:15 +0530

    limit-service spv2 changes commit

--- limits-service/src/main/java/com/in28minutes/microservices/limitsservice/configuration/Configuration.java
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/configuration/Configuration.java
@@ -1,4 +1,4 @@
-package com.in28minutes.microservices.limitsservice;
+package com.in28minutes.microservices.limitsservice.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -6,24 +6,23 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties("limits-service")
 public class Configuration {
-	
 	private int minimum;
 	private int maximum;
-
-	public void setMinimum(int minimum) {
-		this.minimum = minimum;
-	}
-
-	public void setMaximum(int maximum) {
-		this.maximum = maximum;
-	}
 
 	public int getMinimum() {
 		return minimum;
 	}
 
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
 	public int getMaximum() {
 		return maximum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
 	}
 
 }
--- limits-service/src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
+++ limits-service/src/test/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -1,16 +1,13 @@
 package com.in28minutes.microservices.limitsservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class LimitsServiceApplicationTests {
+class LimitsServiceApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
